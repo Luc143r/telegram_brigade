6d3c3ad3dd779dc8d4962f2e262889befbe324f6
Luc143r <48052307+Luc143r@users.noreply.github.com>
2023-12-29 18:52:49 +0300

    Update config.py
    change on .env

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-owner_token_bot = '5798220262:AAHtEEsPR5Mpdf-7WM8GBVUcw9E7ygUybCs'
-user_token_bot = '5528846635:AAEEhu3Dv36Km-b9568M-_HzzYsB1wTGZs8'
-admin_token_bot = '5656882826:AAFKov5owodgNVqkdsRnT_fhcjzXbaCg7NU'
+owner_token_bot = ''
+user_token_bot = ''
+admin_token_bot = ''
 
 data_db = {
     'host': 'localhost',
-    'user': 'root',
-    'password': '~~~~',
+    'user': '',
+    'password': '',
     'database': 'building_bot'
 }
 
